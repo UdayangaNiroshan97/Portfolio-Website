5b3852ac6f0919ef969c3708e328977ea29064a0
UdayangaNiroshan97 <88239066+UdayangaNiroshan97@users.noreply.github.com>
2021-08-27 14:12:31 +0530

    Add files via upload

---
+++ build/precache-manifest.628632d74dfa23a4cdbfdb30379967db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e77e99dc49829eb27e542d7ae4393e",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "5fde1f875ebd546c45a8",
+    "url": "/Portfolio-Website/static/css/main.c35e5d1f.chunk.css"
+  },
+  {
+    "revision": "0f012ec24b8cb32db16c",
+    "url": "/Portfolio-Website/static/js/2.03195eba.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Portfolio-Website/static/js/2.03195eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fde1f875ebd546c45a8",
+    "url": "/Portfolio-Website/static/js/main.04b3ff4b.chunk.js"
+  },
+  {
+    "revision": "0b220200448aef6bbd14",
+    "url": "/Portfolio-Website/static/js/runtime-main.46c74610.js"
+  }
+]);
